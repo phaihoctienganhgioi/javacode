dd0b610238ff18d175cc16f83a9fe484ad1b6792
phaihoctienganhgioi <nnkp300473@gmail.com>
2024-10-24 17:35:59 +0700

    Update fncJavaCodeEnglish.js

--- fncJavaCodeEnglish.js
+++ fncJavaCodeEnglish.js
@@ -1,4 +1,470 @@
- function lietkeCacCauTiengAnh(data) {
+ var myArray = [
+    {
+      	'idcau':'100c01',
+      	'tencau':'Well done!',
+      	'cachdoc':'[queo-đần]',
+      	'phienam':'[wel-dʌn]',
+  		'dichnghia':'Làm tốt lắm, giỏi lắm (dùng để khen ai đó)',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/01.mp3?raw=true'
+  	
+    },
+      {
+      	'idcau':'100c02',
+      	'tencau':'Come on!',
+      	'cachdoc':'[kơm-mon]',
+      	'phienam':'[kʌm-ɑːn]',
+  		'dichnghia':'Thôi nào, thôi mà, cố lên (khi khích lệ người khác)',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/02.mp3?raw=true'
+  	
+    },
+      {
+      	'idcau':'100c03',
+      	'tencau':' Good job!',
+      	'cachdoc':'[guốt-chọp-b]',
+      	'phienam':'[ɡʊd-dʒɑːb]',
+  		'dichnghia':'Làm tốt lắm (khi khen ai đó)',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/03.mp3?raw=true'
+  	
+    },
+      {
+      	'idcau':'100c04',
+      	'tencau':'Not bad!',
+      	'cachdoc':'[nót-be-đ]',
+      	'phienam':'[nɑːt-bæd]',
+  		'dichnghia':'Không tệ, cũng được (Nói về chất lượng, sức khỏe, khen)',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/04.mp3?raw=true'
+  	
+    },
+      {
+      	'idcau':'100c05',
+      	'tencau':'How much?',
+      	'cachdoc':'[hao-mất-ch]',
+      	'phienam':'[haʊ-mʌtʃ]',
+  		'dichnghia':'Bao nhiêu? (Dùng cho danh từ KHÔNG đếm được như thời gian, gạo, nước...)',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/05.mp3?raw=true'
+  	
+    },
+    
+    {
+      	'idcau':'100c05',
+      	'tencau':'How many?',
+       	'phienam':'[haʊ-ˈmen.i]',
+      	'cachdoc':'[hao-mén-nì]',
+  		'dichnghia':'Bao nhiêu? (Dùng cho danh từ ĐẾM ĐƯỢC như xe, người, giày dép...)',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/06.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'Just kidding!',
+       	'phienam':'[dʒʌst-kɪdɪŋ]',
+      	'cachdoc':'[chớt-x-kít-đìn]',
+  		'dichnghia':'Đùa thôi, giỡn đó.',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/07.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'Hold on.',
+       	'phienam':'[hoʊld-ɑːn]',
+      	'cachdoc':'[hâu-đon]',
+  		'dichnghia':'Chờ chút, giữ máy (điện thoại).',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/08.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'Im lost.',
+       	'phienam':'[aɪm-lɑːst]',
+      	'cachdoc':'[am-lót-x-t]',
+  		'dichnghia':'Tôi bị lạc rồi.',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/09.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'Not yet.',
+       	'phienam':'[nɑːt-jet]',
+      	'cachdoc':'[nót-dẹt-t]',
+  		'dichnghia':'Chưa, chưa đâu.',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/10.mp3?raw=true'
+  	
+    },
+    
+    
+    {
+      	'idcau':'100c05',
+      	'tencau':'Me too!',
+       	'phienam':'[miː-tuː]',
+      	'cachdoc':'[mi-tu]',
+  		'dichnghia':'Tôi cũng vậy (ý chỉ người nói cũng đồng tình với ý của đối phương)',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/11.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'Me neither!',
+       	'phienam':'[miː-ˈniː.ðɚ]',
+      	'cachdoc':'[mi-ní-đờ]',
+  		'dichnghia':'Tôi cũng không, tôi cũng chưa!',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/12.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'Try your best!',
+       	'phienam':'[traɪ-jʊr-best]',
+      	'cachdoc':'[trai-doa-bét-x-t]',
+  		'dichnghia':'Cố gắng hết sức nha!',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/13.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'I agree!',
+       	'phienam':'[aɪ-əˈɡriː]',
+      	'cachdoc':'[ai-ờ-g-ri]',
+  		'dichnghia':'Tôi đồng ý!',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/14.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'Be quiet!',
+       	'phienam':'[biː-ˈkwaɪ.ət]',
+      	'cachdoc':'[bi-khoái-ợt-t]',
+  		'dichnghia':'Hãy yên lặng!',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/15.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'No way!',
+       	'phienam':'[noʊ-weɪ]',
+      	'cachdoc':'[nơu-quây]',
+  		'dichnghia':'Không đời nào! (Không thể nào)',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/16.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':' Have fun!',
+       	'phienam':'[hæv-fʌn]',
+      	'cachdoc':'[ha-v-fun]',
+  		'dichnghia':'Chúc vui vẻ (chúc khi đi chơi).',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/17.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'Dont be mad at me!',
+       	'phienam':'[doʊnt-biː-mæd-æt-miː]',
+      	'cachdoc':'[đân-bi-má-đạt-mì]',
+  		'dichnghia':'Đừng giận tôi nha!',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/18.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'My treat.',
+       	'phienam':'[maɪ-triːt]',
+      	'cachdoc':'[mai-tri-t]',
+  		'dichnghia':'Tôi mời, để tôi đãi (khi bạn muốn trả tiền mời người khác).',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/19.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'As soon as possible.',
+       	'phienam':'[æz-suːn-æz-ˈpɑː.sə.bəl]',
+      	'cachdoc':'[ẹc-xun-ẹc-pó-sơ-bồ]',
+  		'dichnghia':'Sớm nhất có thể (chỉ việc cố gắng hoàn thành và rút ngắn thời gian làm việc).',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/20.mp3?raw=true'
+  	
+    },
+
+    {
+      	'idcau':'100c05',
+      	'tencau':'Lets go!',
+       	'phienam':'[lets-ɡoʊ]',
+      	'cachdoc':'[lét-x-gơu]',
+  		'dichnghia':'Chúng ta đi, đi nào, đi thôi',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/21.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'Im home.',
+       	'phienam':'[aɪm-hoʊm]',
+      	'cachdoc':'[am-hôm]',
+  		'dichnghia':'Tôi về nhà rồi đây.',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/22.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'Sorry, Im late.',
+       	'phienam':'[ˈsɔːr.i-aɪm-leɪt]',
+      	'cachdoc':'[xó-rì-am-lây-t]',
+  		'dichnghia':'Xin lỗi, tôi tới muộn.',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/23.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'This way!',
+       	'phienam':'[ðɪs-weɪ]',
+      	'cachdoc':'[díc-x-quây]',
+  		'dichnghia':'Lối này, đường này!',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/24.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'Sooner or later.',
+       	'phienam':'[suːn-ɔːr-ˈleɪ.t̬ɚ]',
+      	'cachdoc':'[xú-nơ-ô-lấy-đờ]',
+  		'dichnghia':'Sớm hay muộn thôi, sớm muộn gì cũng.',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/25.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'Bottoms up!',
+       	'phienam':'[ˈbɑː.t̬əms-ʌp]',
+      	'cachdoc':'[bó-đờm-giớp]',
+  		'dichnghia':'Cạn ly nào!',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/26.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'After you.',
+       	'phienam':'[ˈæf.tɚ-juː]',
+      	'cachdoc':'[áp-tơ-diu]',
+  		'dichnghia':'Bạn trước đi, nhường đường hay mời ai ăn uống trước.',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/27.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':' Its up to you.',
+       	'phienam':'[ɪts-ʌp-tuː-juː]',
+      	'cachdoc':'[ịt-xấp-tu-diu]',
+  		'dichnghia':'Tùy bạn thôi (ý chỉ việc tùy ý quyết định của ai đó).',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/28.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'Congratulations!',
+       	'phienam':'[kənˌɡrætʃ.əˈleɪ.ʃəns]',
+      	'cachdoc':'[kờn-g-ra-chu-lấy-sần-x]',
+  		'dichnghia':'Xin chúc mừng!',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/29.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'Follow me!.',
+       	'phienam':'[ˈfɑː.loʊ-miː]',
+      	'cachdoc':'[phó-lờu-mì]',
+  		'dichnghia':'Đi theo tôi! (Dẫn đường cho ai đó đi theo mình)',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/30.mp3?raw=true'
+  	
+    },
+    
+
+     {
+      	'idcau':'100c05',
+      	'tencau':'Easier said than done.',
+       	'phienam':'[]',
+      	'cachdoc':'[í-zì-ờ-séc-đèn-đân]',
+  		'dichnghia':'Nói dễ hơn làm, nói thì dễ, làm mới khó.',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/31.mp3?raw=true'
+  	
+    },
+     {
+      	'idcau':'100c05',
+      	'tencau':'Forget it!',
+       	'phienam':'[]',
+      	'cachdoc':'[phơ-ghét-dịc-t]',
+  		'dichnghia':'Quên nó đi! Điều đó không quan trọng. Hoặc khi không muốn giải thích thêm điều gì đó nữa.',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/32.mp3?raw=true'
+  	
+    },
+     {
+      	'idcau':'100c05',
+      	'tencau':'Im glad to hear that.',
+       	'phienam':'[]',
+      	'cachdoc':'[am-g-lát-tu-hia-đạt-t]',
+  		'dichnghia':'Tôi rất vui khi nghe điều đó (chia sẻ niềm vui khi nhận được tin tức tốt của người khác).',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/33.mp3?raw=true'
+  	
+    },
+     {
+      	'idcau':'100c05',
+      	'tencau':'The more the merrier.',
+       	'phienam':'[]',
+      	'cachdoc':'[đờ-mo-đờ-mé-ri-ờ]',
+  		'dichnghia':'Càng đông càng vui.',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/34.mp3?raw=true'
+  	
+    },
+     {
+      	'idcau':'100c05',
+      	'tencau':'No problem.',
+       	'phienam':'[]',
+      	'cachdoc':'[nơu-p-róp-lờm]',
+  		'dichnghia':'Không thành vấn đề. Không có gì.',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/35.mp3?raw=true'
+  	
+    },
+     {
+      	'idcau':'100c05',
+      	'tencau':'Good luck!',
+       	'phienam':'[]',
+      	'cachdoc':'[guốt-lất-k]',
+  		'dichnghia':'Chúc may mắn!',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/36.mp3?raw=true'
+  	
+    },
+     {
+      	'idcau':'100c05',
+      	'tencau':'Pay attention to me, please!.',
+       	'phienam':'[]',
+      	'cachdoc':'[pây-ờ-tén-sần-tu-mi-p-li-s]',
+  		'dichnghia':'Vui lòng chú ý đến tôi! (Khi muốn người khác chú ý đến mình)',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/37.mp3?raw=true'
+  	
+    },
+     {
+      	'idcau':'100c05',
+      	'tencau':'I promise.',
+       	'phienam':'[]',
+      	'cachdoc':'[ai-p-ró-mịt-x]',
+  		'dichnghia':'Tôi hứa mà.',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/38.mp3?raw=true'
+  	
+    },
+     {
+      	'idcau':'100c05',
+      	'tencau':'Same here.',
+       	'phienam':'[]',
+      	'cachdoc':'[xem-hia]',
+  		'dichnghia':'Tôi cũng vậy.',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/39.mp3?raw=true'
+  	
+    },
+    
+        
+     {
+      	'idcau':'100c05',
+      	'tencau':'Same to you.',
+       	'phienam':'[]',
+      	'cachdoc':'[xem-tơ-diu]',
+  		'dichnghia':'Bạn cũng thế nhé.',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/40.mp3?raw=true'
+  	
+    },
+    
+    
+     {
+      	'idcau':'100c05',
+      	'tencau':'Its none of your business.',
+       	'phienam':'[]',
+      	'cachdoc':'[ít-x-nân-ọp-do-bía-x-nịt-x]',
+  		'dichnghia':'Không phải chuyện của bạn.',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/41.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'Of course!',
+       	'phienam':'[]',
+      	'cachdoc':'[ộp-ko-x]',
+  		'dichnghia':'Dĩ nhiên rồi!',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/42.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'Enjoy your meal.',
+       	'phienam':'[]',
+      	'cachdoc':'[ìn-chói-do-miêu]',
+  		'dichnghia':'Chúc bạn ngon miệng.',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/43.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'Take it easy.',
+       	'phienam':'[]',
+      	'cachdoc':'[tây-kịt-í-gì]',
+  		'dichnghia':'Bình tĩnh, từ từ.',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/44.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'Slow down.',
+       	'phienam':'[]',
+      	'cachdoc':'[x-lơu-đao]',
+  		'dichnghia':'Chậm lại, chậm lại chút đi (về chạy xe).',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/45.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'Speed up.',
+       	'phienam':'[]',
+      	'cachdoc':'[x-pi-đập]',
+  		'dichnghia':'Nhanh lên, tăng tốc.',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/46.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'Im almost there.',
+       	'phienam':'[]',
+      	'cachdoc':'[am-ó-mơu-x-đe]',
+  		'dichnghia':'Tôi gần đến nơi rồi (khi có ai hỏi đến nơi chưa).',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/47.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'Take care.',
+       	'phienam':'[]',
+      	'cachdoc':'[tấy-ke]',
+  		'dichnghia':'Bảo trọng nha.',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/48.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'Money talks.',
+       	'phienam':'[]',
+      	'cachdoc':'[mớn-ni-thọt-x]',
+  		'dichnghia':'Có tiền là có quyền. Có tiền mua tiên cũng được.',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/49.mp3?raw=true'
+  	
+    },
+    {
+      	'idcau':'100c05',
+      	'tencau':'Try again.',
+       	'phienam':'[]',
+      	'cachdoc':'[troai-ờ-gen]',
+  		'dichnghia':'Thử lại đi. Lại lần nữa đi.',
+       	'linkaudio':'https://github.com/phaihoctienganhgioi/100caucuamiengmp3/blob/main/50.mp3?raw=true'
+  	
+    },
+    
+  ]
+  
+  lietkeCacCauTiengAnh(myArray);
+  
+  function lietkeCacCauTiengAnh(data) {
     var treeCacCauTiengAnh = document.getElementById('tree');
     var row = '';
     for (var i=0; i<data.length; i++) {
@@ -35,7 +501,10 @@
     treeCacCauTiengAnh.innerHTML += row;
   }
   
-  function playMusic(elem, laplai) {
+  
+  
+		var music = new Audio();
+  		function playMusic(elem, laplai) {
           	//alert("gfdghfdgfdg");
           	//var id = $(elem).attr("id");
     		//alert(id);
@@ -54,3 +523,6 @@
           }
           	
         }
+  
+
+  		
